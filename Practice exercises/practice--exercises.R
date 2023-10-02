07aea4c38f81c2a0559a27318cf0faed44a00ee9
olivianicastro <nolivia@sas.upenn.edu>
2023-10-02 14:59:00 -0400

    Exam #1

--- Practice exercises/practice--exercises.R
+++ Practice exercises/practice--exercises.R
@@ -1,6 +1,6 @@
 
 library(tidyverse)
-income.data <- read_csv("/Users/olivianicastro/Desktop/2023-2024 Fall/crim1200-stat/GitHub/crim1200-stat/data/income.data.csv")
+crime.dat <- read_csv("/Users/olivianicastro/Desktop/2023-2024 Fall/crim1200-stat/GitHub/crim1200-stat/data/crime.dat.csv")
 
-income.data
+crime.dat
 
